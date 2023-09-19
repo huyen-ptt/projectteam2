1e8e20922b7a6ee433c7c10c7bf3609c0f139578
Thanh Huyen <phamthanhhuyen19124>
2023-09-19 17:40:52 +0700

    chỉnh db

--- src/main/java/database/database.sql
+++ src/main/java/database/database.sql
@@ -4,17 +4,17 @@ USE khutro;
 
 -- Tạo bảng User nếu nó chưa tồn tại
 CREATE TABLE IF NOT EXISTS User (
-  id INT PRIMARY KEY,
-  username NVARCHAR(255) NOT NULL,
-  password NVARCHAR(255) NOT NULL,
-  name VARCHAR(255),
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  username VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  name NVARCHAR(255),
   phone INT,
-  created_time INT
+  created_time datetime
 );
 
 -- Tạo bảng Room nếu nó chưa tồn tại
 CREATE TABLE IF NOT EXISTS Room (
-  id INT PRIMARY KEY,
+  id INT PRIMARY KEY AUTO_INCREMENT,
   room_name NVARCHAR(255),
   room_description TEXT,
   price FLOAT,
@@ -22,7 +22,7 @@ CREATE TABLE IF NOT EXISTS Room (
   status BOOLEAN,
   room_area DECIMAL(10, 2),
   type VARCHAR(255),
-  created_time INT
+  created_time datetime
 );
 
 -- Tạo bảng RoomImages nếu nó chưa tồn tại
@@ -30,13 +30,13 @@ CREATE TABLE IF NOT EXISTS RoomImages (
   id INT PRIMARY KEY AUTO_INCREMENT,
   image_url VARCHAR(255),
   room_id INT,
-  created_time INT,
+  created_time date,
   FOREIGN KEY (room_id) REFERENCES Room(id)
 );
 
 -- Tạo bảng HoaDon nếu nó chưa tồn tại
 CREATE TABLE IF NOT EXISTS HoaDon (
-  id INT PRIMARY KEY,
+  id INT PRIMARY KEY AUTO_INCREMENT,
   tien_dien INT,
   tien_nuoc INT,
   tien_dich_vu INT,
@@ -45,7 +45,7 @@ CREATE TABLE IF NOT EXISTS HoaDon (
 
 -- Tạo bảng Customers nếu nó chưa tồn tại
 CREATE TABLE IF NOT EXISTS Customers (
-  id INT PRIMARY KEY,
+  id INT PRIMARY KEY AUTO_INCREMENT,
   name NVARCHAR(255),
   room_id INT,
   phone INT,
